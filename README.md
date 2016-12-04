126437e7b1590f7efe8323243965728779852d09
Mark Markov <markymarkov@gmail.com>
2016-12-05 00:55:30 +0200

    Initial Upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# gulp-sass-pug-browsersync
+
+# To install
+
+1. Ruby installer ([windows](http://rubyinstaller.org/downloads/))- ruby
+2. NodeJS - use the installer
+3. GulpJS - $ npm install -g gulp
+4. npm install - in work folder
+5. gulp - Run.
---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+'use strict';
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+
+const reload = browserSync.reload;
+
+
+//Compile SASS files to CSS
+gulp.task('sass', function () {
+  return gulp.src('./assets/css/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 15 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./site/css'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+// Compile PUG files to HTML
+gulp.task('pug', () =>{
+  return gulp.src('_pugfiles/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('./_include'))
+    .pipe(gulp.dest('./site'))
+})
+
+gulp.task('js', function () {
+  gulp.src("./assets/js/function.js")
+  .pipe(rename("js/function.js"))
+  .pipe(gulp.dest("./site"));
+});
+
+// Working directory
+gulp.task('browser-sync', ['sass', 'pug', 'js'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./site"
+        }
+    });
+});
+
+// Watch files compiling
+gulp.task('watch', function () {
+  gulp.watch('./assets/css/*.sass', ['sass']);
+  gulp.watch('./_pugfiles/*.pug', ['pug']);
+  gulp.watch('./site/*.html').on('change', reload);
+  gulp.watch('./assets/js/*.js', ['js']);
+  gulp.watch('./site/js/*.js').on('change', reload);
+});
+
+gulp.task('default', ['watch', 'browser-sync']);
